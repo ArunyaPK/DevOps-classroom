0bfac5f55cac904fe08df58f930ff975f231cf69
ArunyaPK <arunyamalar@gmail.com>
2023-05-21 16:30:53 +0530

    added comments

--- task_01.bash
+++ task_01.bash
@@ -1,9 +1,11 @@
+#Create 20 files into .txt files
 touch file{1..20}.txt
 
+# Rename top 5 files into .yml extension
 for i in {1..5}; 
 do
   mv file${i}.txt file${i}.yml
 done
 
-
+# List top 5 modified files 
 ls -lt | head -n 5 
